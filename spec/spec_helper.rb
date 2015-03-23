401601ea96234621fb3a834df3bde5748e82bf2c
Hank Beaver <hbeaver@gmail.com>
2015-03-23 16:30:22 -0400

    Remove pry from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 $LOAD_PATH.unshift File.expand_path('../support', __FILE__)
-require 'pry'
 require 'serial_spec'
 
 if defined?(ActiveModel::Serializer)
