5329f09b1d39c270a80332ccf743aff99bd087f7
Blake Chambers <chambb1@gmail.com>
2015-04-30 22:04:24 -0400

    Updates include_provide_matcher

--- lib/serial_spec/request_response/include_provide_matcher.rb
+++ lib/serial_spec/request_response/include_provide_matcher.rb
@@ -1,7 +1,7 @@
 RSpec::Matchers.define :include_a_provided do |expected|
   match do |actual_list|
     actual_list.detect do |actual|
-      SerialSpec::RequestResponse::ProvideMatcher::Provide.new(expected).matches?(actual)
+      SerialSpec::RequestResponse::ProvideMatcher::Provide.new(self, expected).matches?(actual)
     end
   end
 end
